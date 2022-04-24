c493af2f90546cd57853e708319ed2ef1c700af6
guilhermefrontdev <guilherme.frontdev@hotmail.com>
2022-04-23 23:54:14 -0300

    correção e centralização do conteudo

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -16,24 +16,38 @@ function verificar() {
             if (idade >=0 && idade < 10) {
                  // Criança
                  img.setAttribute('src', 'image/boy.png')
+                 img.style.height = '200px'
             } else if (idade < 21) {
                 // Jovem
-                img.setAttribute('src')
+                img.setAttribute('src', 'image/teen-boy.png')
+                img.style.height = '200px'
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'image/men.png')
+                img.style.height = '200px'
             } else {
                 // Idoso
+                img.setAttribute('src', 'image/grandpa.png')
+                img.style.height = '200px'
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'image/girl.png')
+                img.style.height = '200px'
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'image/teen-girl.png')
+                img.style.height = '200px'
             } else if (idade < 50) {
                 // Adulta
+                img.setAttribute('src', 'image/woman.png')
+                img.style.height = '200px'
             } else {
                 // Idosa
+                img.setAttribute('src', 'image/grandma.png')
+                img.style.height = '200px'
             }
         }
         res.style.textAlign = 'center'
