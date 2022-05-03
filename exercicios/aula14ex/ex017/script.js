a0bda555ef9057722d3700685d339183b45c594e
guilhermefrontdev <guilherme.frontdev@hotmail.com>
2022-05-03 14:42:34 -0300

    exercicio tabuada

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Testando JS!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 20) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
